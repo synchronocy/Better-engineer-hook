f3373fb87132360553679b9f52de9e93848dac71
John <29266648+synchronocy@users.noreply.github.com>
2018-12-23 22:51:05 +1100

    Update athena.lua

--- athena.lua
+++ athena.lua
@@ -380,13 +380,10 @@ function ESP(v) -- courtesy of pasteware
 end
 function staff()
 	function ad(ply)
-		local zOffset = 50 // unsure why we offset by 50 
-		local x = ply:GetPos().x			//Get the X position of our player
-		local y = ply:GetPos().y			//Get the Y position of our player
-		local z = ply:GetPos().z			//Get the Z position of our player
-		local pos = Vector(x,y,z+zOffset)	//Add our offset onto the Vector
-		local pos2d = pos:ToScreen()		//Change the 3D vector to a 2D one
-		draw.DrawText("Admin","Default",pos2d.x,pos2d.y,Color(255,255,255,255),TEXT_ALIGN_CENTER)	//Draw the indicator
+		local cords = ply:GetPos()
+		local x,y,z = cords.x,cords.y,cords.z
+		local pos = Vector(x,y,z+50):ToScreen()		
+		draw.DrawText("Admin","Default",pos.x,pos.y,Color(255,255,255,255),TEXT_ALIGN_CENTER)	//Draw the indicator
 	end
 	function sad(ply)
 		local zOffset = 50 // unsure why we offset by 50 
